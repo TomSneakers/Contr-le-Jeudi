05cf76b3056f8bb880d8f719ffde01c63c7180d3
Tom <tomdesvignes031@gmail.com>
2023-06-15 13:45:24 +0200

    first

---
+++ Contrôle jeudi/Contrôle jeudi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Contrôle jeudi
+//
+//  Created by Tom Desvignes on 15/06/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
